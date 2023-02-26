a4e658b5d390ee3edc4d6ded0f0ab12df0aa0195
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-27 00:19:37 +0300

    Принимаем число (А) и считаем сумму цифра заданного числа

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine(CalculateSumToN(ReadInt("Введите число")));
+
+int ReadInt(string argument)  // функция проверяет ввели ли мы целое число до тех пор,
+{                              // пока мы не введем целове число
+  Console.Write($"{argument}: ");
+  int number;
+
+  while(!int.TryParse(Console.ReadLine(), out number))
+  {
+    Console.WriteLine("Вы ввели не число!");
+  }
+
+  return number;
+}
+
+int CalculateSumToN(int number)
+{
+  int sum = 0;
+
+  for(int i = 1; i <= number; i++)
+  {
+    sum = sum + i;
+  }
+
+  return sum;
+}
