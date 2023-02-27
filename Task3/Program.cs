a59723ae85188e7f118de5650332022fc58ed725
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-27 20:47:49 +0300

    Задаем массив из 8 элементов и выводим на экран

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+ void NewArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+
+  while(index < length)
+  {
+    collection[index] = new Random().Next(1, 8);
+    index++;
+  }
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+
+  while(position < count)
+  {
+    Console.Write($"{col[position] + " "}");
+    position++;
+  }
+}
+
+int[] array = new int[8];
+
+NewArray(array);
+PrintArray(array);
